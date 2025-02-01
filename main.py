9bc7cbe016ac0203bede3fe803ddc59ebd8e9080
Ivan Zuev <ivan78.ru@yandex.ru>
2025-02-02 00:52:57 +0300

    Refactoring

--- main.py
+++ main.py
@@ -7,32 +7,32 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-token = os.getenv('TG_TOKEN')
-main_chat_id = os.getenv('TG_CHAT_ID')
-if main_chat_id:
-    main_chat_id = int(main_chat_id)
+TG_TOKEN = os.getenv('TG_TOKEN')
+TG_NOTIFY_CHAT_ID = os.getenv('TG_NOTIFY_CHAT_ID')
+if TG_NOTIFY_CHAT_ID:
+    TG_NOTIFY_CHAT_ID = int(TG_NOTIFY_CHAT_ID)
 
-if not token:
+if not TG_TOKEN:
     # for dev
     print('Токен не задан')
-    token = '1234567899:AA***'
-    main_chat_id = 123456789
+    TG_TOKEN = '1234567899:AA***'
+    TG_NOTIFY_CHAT_ID = 123456789
 
-bot = Bot(token=token)
+bot = Bot(token=TG_TOKEN)
 
 
 class SMSRequest(BaseModel):
-    sender: str  # Номер отправителя
-    message: str  # Текст сообщения
-    who: str | None = None  # Кто отправил (необязательно)
-    timestamp: Optional[int | str] = None  # Время отправки (необязательно)
+    sender: str
+    message: str
+    who: str | None = None
+    timestamp: Optional[int | str] = None
 
 
 @app.post("/receive-sms/")
 async def receive_sms(data: SMSRequest):
     print(f"Получено SMS {data}")
     text = f'Кто: {data.who}\nОтправитель: {data.sender}\n\n{data.message}'
-    await bot.send_message(main_chat_id, text)
+    await bot.send_message(TG_NOTIFY_CHAT_ID, text)
 
     return {"status": "SMS получено"}
 
